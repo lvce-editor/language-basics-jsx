c33cac9647f0e824a2c9edaf1212682d32b8f535
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2023-01-31 15:50:20 +0100

    fix: highlighting of tagged template function calls (#35)

--- src/tokenizeJsx.js
+++ src/tokenizeJsx.js
@@ -124,7 +124,7 @@ const RE_STRING_DOUBLE_QUOTE_CONTENT = /^[^"\\]+/
 const RE_STRING_BACKTICK_QUOTE_CONTENT = /^[^`\\\$]+/
 const RE_STRING_ESCAPE = /^\\./
 const RE_SHEBANG = /^#!.*/
-const RE_FUNCTION_CALL_NAME = /^[\w]+(?=\s*(\(|\=\s*function|\=\s*\())/
+const RE_FUNCTION_CALL_NAME = /^[\w]+(?=\s*(\`|\(|\=\s*function|\=\s*\())/
 const RE_DECORATOR = /^@\w+/
 const RE_BACKSLASH = /^\\/
 const RE_DOLLAR_CURLY_OPEN = /^\$\{/
